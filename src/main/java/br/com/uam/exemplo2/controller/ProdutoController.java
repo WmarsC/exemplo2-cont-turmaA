7abc5c983bac3ea8c2d0fa10aa5c966bc92e399d
Luigi <luigitorlaiokano@gmail.com>
2026-09-09 14:36:57 +0000

    crud

--- src/main/java/br/com/uam/exemplo2/controller/ProdutoController.java
+++ src/main/java/br/com/uam/exemplo2/controller/ProdutoController.java
@@ -1,9 +1,15 @@
 package br.com.uam.exemplo2.controller;
 
 import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +17,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.uam.exemplo2.model.Produto;
 import br.com.uam.exemplo2.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -34,6 +42,54 @@ public class ProdutoController {
                             .toUri();
 
         return ResponseEntity.created(location).body(produtoSalvo);
+    }
+
+    @GetMapping 
+    public List<Produto>ListarTodos() {
+        return repository.findAll();
+    }
+    
+    @GetMapping("/{id}") 
+    public ResponseEntity<Produto> buscarProId(@PathVariable Long id) {
+        Optional<Produto> produtoBuscado = repository.findById(id);
+
+        if (produtoBuscado.isPresent()) {
+            return ResponseEntity.ok(produtoBuscado.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+     @PutMapping("/{id}") 
+    public ResponseEntity<Produto> atuaizarProduto(@PathVariable Long id,
+                                    @RequestBody Produto produtoAtualizado )
+    {
+        Optional<Produto> produtoBuscado = repository.findById(id);
+
+        if (produtoBuscado.isPresent()) {
+            Produto produtoExistente = produtoBuscado.get()
+            produtoExistente.setNome(produtoAtualizado.getNome());
+            produtoExistente.setPreco(produtoAtualizado.getPreco());
+
+            Produto produtoSalvo = repository.save(produtoExistente);
+            return ResponseEntity.ok(produtoSalvo);
+
+          return ResponseEntity.ok(produtoBuscado.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}") 
+    public ResponseEntity<Void> apagarProduto(@PathVariable Long id) {
+        Optional<Produto> produtoBuscado = repository.findById(id);
+
+        if (produtoBuscado.isPresent()) {
+            repository.deleteAllById(id);
+            return ResponseEntity.noContent().build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
